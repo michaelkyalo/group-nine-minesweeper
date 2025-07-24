79115be0b7e8c12251d2e19ea7fc487c746b17fb
michaelkyalo <michaelkyalo876@gmail.com>
2025-07-24 12:19:05 +0300

    cssstyling

--- minesweeper/src/main.jsx
+++ minesweeper/src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import App from '../App.jsx'
 
+import "./mycss/css.css"
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
